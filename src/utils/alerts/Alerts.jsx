2c1e917184811d6ab6c657457d8d5f880d99c158
BrawlBreed <99895434+BrawlBreed@users.noreply.github.com>
2024-03-13 04:07:53 +0200

    a

--- src/utils/alerts/Alerts.jsx
+++ src/utils/alerts/Alerts.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
+//
 export const Alerts = ({ success, msg }) => {
   const [active, setActive] = useState(false);
   const redirect = useNavigate()
